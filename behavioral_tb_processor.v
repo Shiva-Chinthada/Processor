506d210aa2e1c7f76dfc4d1aa273df2bbc5c41e1
Shiva-Chinthada <chintadavenkatashivadurgarao@gmail.com>
2025-06-30 06:33:32 +0530

    Create behavioral_tb_processor.v

---
+++ behavioral_tb_processor.v
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<wave_config>
+   <wave_state>
+   </wave_state>
+   <db_ref_list>
+      <db_ref path="tb_pipelined_processor_behav.wdb" id="1">
+         <top_modules>
+            <top_module name="glbl" />
+            <top_module name="tb_pipelined_processor" />
+         </top_modules>
+      </db_ref>
+   </db_ref_list>
+   <zoom_setting>
+      <ZoomStartTime time="0.000 ns"></ZoomStartTime>
+      <ZoomEndTime time="225.001 ns"></ZoomEndTime>
+      <Cursor1Time time="225.000 ns"></Cursor1Time>
+   </zoom_setting>
+   <column_width_setting>
+      <NameColumnWidth column_width="93"></NameColumnWidth>
+      <ValueColumnWidth column_width="111"></ValueColumnWidth>
+   </column_width_setting>
+   <WVObjectSize size="12" />
+   <wvobject fp_name="/tb_pipelined_processor/clk" type="logic">
+      <obj_property name="ElementShortName">clk</obj_property>
+      <obj_property name="ObjectShortName">clk</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/reset" type="logic">
+      <obj_property name="ElementShortName">reset</obj_property>
+      <obj_property name="ObjectShortName">reset</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/pc_out" type="array">
+      <obj_property name="ElementShortName">pc_out[31:0]</obj_property>
+      <obj_property name="ObjectShortName">pc_out[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/instruction_out" type="array">
+      <obj_property name="ElementShortName">instruction_out[31:0]</obj_property>
+      <obj_property name="ObjectShortName">instruction_out[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/cycle_count" type="array">
+      <obj_property name="ElementShortName">cycle_count[31:0]</obj_property>
+      <obj_property name="ObjectShortName">cycle_count[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/test_passed" type="array">
+      <obj_property name="ElementShortName">test_passed[31:0]</obj_property>
+      <obj_property name="ObjectShortName">test_passed[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/total_tests" type="array">
+      <obj_property name="ElementShortName">total_tests[31:0]</obj_property>
+      <obj_property name="ObjectShortName">total_tests[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/expected_reg1" type="array">
+      <obj_property name="ElementShortName">expected_reg1[31:0]</obj_property>
+      <obj_property name="ObjectShortName">expected_reg1[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/expected_reg4" type="array">
+      <obj_property name="ElementShortName">expected_reg4[31:0]</obj_property>
+      <obj_property name="ObjectShortName">expected_reg4[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/expected_reg5" type="array">
+      <obj_property name="ElementShortName">expected_reg5[31:0]</obj_property>
+      <obj_property name="ObjectShortName">expected_reg5[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/expected_reg6" type="array">
+      <obj_property name="ElementShortName">expected_reg6[31:0]</obj_property>
+      <obj_property name="ObjectShortName">expected_reg6[31:0]</obj_property>
+   </wvobject>
+   <wvobject fp_name="/tb_pipelined_processor/expected_reg7" type="array">
+      <obj_property name="ElementShortName">expected_reg7[31:0]</obj_property>
+      <obj_property name="ObjectShortName">expected_reg7[31:0]</obj_property>
+   </wvobject>
+</wave_config>
